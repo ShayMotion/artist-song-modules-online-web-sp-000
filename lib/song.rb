4233547a104e1f7a904a0545d74aff20e3459bea
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-09-03 02:27:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,9 +6,9 @@ class Song
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  def self.all
+    @@songs
+ end
 
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
